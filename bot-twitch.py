50516fb8a22b7560a989a42fe68b8d9eb483f425
Erwan SINCK <erwan.sinck12@gmail.com>
2025-04-25 12:28:39 +0200

    ajout bot twitch

---
+++ bot-twitch.py
@@ -0,0 +1,69 @@
+from twitchio.ext import commands
+import os
+import random 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Bot(commands.Bot):
+
+    def __init__(self):
+        super().__init__(
+            token=os.getenv("TWITCH_TOKEN"),
+            prefix='!',
+            initial_channels=['dhalsiiim']
+        )
+        self.REPO_URL = os.getenv("REPO_URL"),
+
+    async def event_ready(self):
+        print(f'Connecté au chat Twitch en tant que : {self.nick}')
+
+    async def event_message(self, message):
+        if message.echo or message.author.name == "cdeuxs":
+            return
+
+        print(f'{message.author.name}: {message.content}')
+
+        if "quoi" in message.content.lower().split():
+            await message.channel.send(f"@{message.author.name} quoicoubeh")
+        
+        if "caca" in message.content.lower().split():
+            await message.channel.send(f"@{message.author.name} caca ? comme caca2squidgame mon createur!")
+
+        if len(message.content.split()) >= 30:
+            await message.channel.send(f"fiouuu y'en a des mots la @{message.author.name}")
+
+        await self.handle_commands(message)
+
+    @commands.command(name="c2s")
+    async def c2s_command(self, ctx):
+        await ctx.send("C2S powered 😎")
+    
+    @commands.command(name="repo")
+    async def c2s_command(self, ctx):
+        await ctx.send(f"Voici le repo du bot : {self.REPO_URL}")
+
+    @commands.command(name="boule")
+    async def boule_de_cristal(self, ctx):
+        reponses = [
+            "Oui, clairement ! 🔮",
+            "Non, sûrement pas. ❌",
+            "Les astres sont confus... 🤔",
+            "Probablement oui ! 🌟",
+            "Hmm... c'est un grand OUI du destin. ✅",
+            "Pas pour l’instant, mais persévère ! 💪",
+            "C’est écrit dans les étoiles... peut-être 🌠"
+        ]
+
+        await ctx.send(f"@{ctx.author.name} {random.choice(reponses)}")
+
+    @commands.command(name="fesses")
+    async def c2s_command(self, ctx):
+        if ctx.author.name == "caca2squidgame" or ctx.author.name == "dhalsiiim":
+            await ctx.send("SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss SSSsss")
+        else :
+            await ctx.send("fesses")
+
+
+bot = Bot()
+bot.run()
